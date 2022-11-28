bee40366722f86026ea590d4f09ec38055e5d971
Ryan Kulp <ryanckulp@gmail.com>
2022-11-27 23:36:24 -0500

    add back postmark token ENV for possible mailers

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module TenKHours
     config.action_mailer.default_url_options = { host: ENV['BASE_URL'] }
     config.action_mailer.default_options = { from: ENV['ADMIN_EMAIL'] }
     config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { api_token: ENV['postmark_api_token'] }
+    config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
 
     # serve images from asset pipeline in mailers
     config.asset_host = ENV['BASE_URL']
