e39861b84ee02d6a38e29c26d9c7e407c5308333
liyicky <jason@liyicky.com>
2022-11-29 21:19:16 +0900

    Let you setup the stub project in the application.yml. Reminds you to setup the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 # create User
 user = User.first
-User.create!(email: 'admin@10khours.to', password: 'password') if user.nil?
+User.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD']) if user.nil?
 
 # stub Project
 project = Project.first
 
 if project.nil?
   Project.create!(
-      name: 'Kulp Estates',
-      tagline: 'Tech entrepreneur learning to farm, raise livestock, and build a compound for experiential networking retreats.',
-      description: "<a class=\"underline\" href=\"https://github.com/ryanckulp/10k_hours\" target=\"_blank\">10k Hours</a> is a live changelog of improvements to Kulp Estates, a 50 acre ranch outside Atlanta, GA.",
-      external_url: 'https://twitter.com/ryanckulp',
+      name: ENV['PROJECT_NAME'],
+      tagline: ENV['TAGLINE'],
+      description: ENV['DESCRIPTION'],
+      external_url: ENV['EXTERNAL_URL']
   )
 end
