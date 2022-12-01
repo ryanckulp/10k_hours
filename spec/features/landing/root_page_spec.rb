97afca74afa884bf907d1f4a8af008766e262efe
liyicky <jason@liyicky.com>
2022-12-01 14:44:46 +0900

    specs running

--- spec/features/landing/root_page_spec.rb
+++ spec/features/landing/root_page_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 feature 'Root page', :js => true do
   before do
+    create :project
     visit root_path
   end
 
   scenario 'Root page should work' do
-    expect(page).to have_text 'Contact'
-    expect(page).to have_text 'Login'
-    expect(page).to have_text 'Signup'
-
+    expect(page).to have_text 'Follow Me'
     expect(page).to_not have_text 'Logout'
   end
 end
